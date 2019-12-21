295a70b68e9f9ad94fd272e01ecc6a0560434307
ruthtech <leeruths@gmail.com>
2019-12-21 12:28:12 -0500

    Add contact information to Contact page.

--- ruthtech/src/components/pages/Contact.js
+++ ruthtech/src/components/pages/Contact.js
@@ -1,11 +1,21 @@
 import React from "react";
+import Logo from '../assets/media/ruthtech.svg';
+
 
 function Contact(props) {
   return (
     <div className="container-fluid">
-      Contact me via LinkedIn or view my GitHub pages.
-      <a href="https://linkedin.com/in/ruthsarahlee">LinkedIn</a>
-      <a href="https://github.com/ruthtech">GitHub</a>
+      <div className="row justify-content-center d-flex">
+        <div className="col-12 col-sm-8">
+          <div className="definition box">
+            <h5 className="text-center mb-3">Contact Me</h5>
+            Send me an email at <span className="loginliteral">leeruths@gmail.com</span> or send a message
+            through my <a href="https://linkedin.com/in/ruthsarahlee">LinkedIn</a>.
+            <p>Looking forward to hearing from you!</p>
+            -- <img src={Logo} alt="Ruth Lee" width="15%" height="15%"/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
