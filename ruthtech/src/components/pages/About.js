f95e68a49579d33f9d00aed9dbf766afc35ec18b
ruthtech <leeruths@gmail.com>
2019-12-20 22:54:33 -0500

    Make Jumbotron on first page show the content of the selected project card

--- ruthtech/src/components/pages/About.js
+++ ruthtech/src/components/pages/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../assets/css/style.css";
+import "../assets/css/about.css";
 
 function About() {
   return (
@@ -9,34 +10,81 @@ function About() {
           <div className="colour-box first">Write</div>
         </div>
         <div className="col-12 col-sm-4">
-          <div className="colour-box second">Clear</div>
+          <div className="colour-box second">Organize</div>
         </div>
         <div className="col-12 col-sm-4">
           <div className="colour-box third">Code</div>
         </div>
       </div>
-    <div className="row justify-content-center d-flex">
+      <div className="row justify-content-center d-flex">
         <div className="col-12 col-sm-8">
-          <div className="definition box bottom-row">
+          <div className="definition box">
             <h5 className="text-center mb-3">I get things done.</h5>
             <p>
               While I stayed home to raise my children I volunteered at their
               school regularly. We submitted grant applications, 
               wrote social media for Council,
               served pizza lunch to over 730 kids, and worked with the TDSB to get
-              Kindergarten yards repaired or replaced.
+              Kindergarten yards repaired.
             </p>
             <p>
               I've coded, tested, built, liaisoned, managed, project managed, and worked
               on teams that spanned companies and continents. Having just completed a bootcamp
-              that taught me Full Stack Web Development, I am interested in learning new technologies
-              and solving problems.
+              that taught me Full Stack Web Development, I will continue to learn new technologies
+              and solve problems.
             </p>
           </div>
         </div>
       </div>
+      <div className="row bottom-row">
+        <SkillsCard skillSets={skillSets} />
+      </div>
     </div>
   );
 }
+
+
+const skillSets = [
+  {
+    title: "Tests and Tools",
+    skills: ["Git", "JUnit", "JEST", "Visual Code", "Figma"],
+    skillColour: 'bg-lightBlue'
+  },
+  {
+    title: "Soft skills",
+    skills: ["Organized", "Reliable", "Problem solver", "Strong work ethic", "Thorough"],
+    skillColour: 'bg-mediumBlue'
+  },
+  {
+    title: "Programming",
+    skills: ["JavaScript", "Java", "SQL", "React", "Node"],
+    skillColour: 'bg-darkBlue'
+  }
+];
+
+
+function SkillsCard(props) {
+  return (
+    props.skillSets.map( (skillSet, index) => {
+      return (
+          <div className="col-12 col-sm-4" key={'skillSet'+index}>		
+            <div className="card skills-card">
+              <div className="card-header portfolio-header">
+                <h6 className="skills-title">{skillSet.title}</h6>
+              </div>
+              <div className={"card-body skills-body " +  skillSet.skillColour}>
+                <ul className="card-text pl-3">
+                  {skillSet.skills.map((skill, index2) => {
+                    return <li key={'skill'+index2}>{skill}</li>;
+                  })}
+                </ul>
+              </div>
+            </div>
+          </div>
+      )})
+  );
+}
+
+
 
 export default About;
--- ruthtech/src/components/pages/Home.js
+++ ruthtech/src/components/pages/Home.js
@@ -1,59 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/css/style.css";
+import PrjImg0 from '../assets/images/trusael-min.jpg';
+import PrjImg1 from '../assets/images/toronto-min.jpg';
+import PrjImg2 from '../assets/images/employee-tracker.mp4';
 
 function Home() {
+  const [activeProjectIndex, setActiveProjectIndex] = useState(0); // Default to projects[0]
+
   const projects = [
     {
-      title: "TrusÆL", 
+      title: "TrusÆL Online Marketplace", 
       summary: "Real estate agents compete by bidding to represent sellers in transactions. Sellers compare bids and look at agent reviews when deciding who will get their business.", 
       skills: ['Server (Node, Express)', 'Heroku', 'dotenv', 'MySQL', 'Crypto-JS', 'JavaScript', 'Bootstrap', 'AJAX', 'jQuery', 'HTML', 'CSS'  ], 
       github: "https://github.com/BCButcher/TRUS-L", 
       heroku: "https://murmuring-cove-22350.herokuapp.com/",
       key: 1,
-      colour: "bg-first"
+      colour: "bg-lightBlue",
+      image: {PrjImg0},
+      imageName: "PrjImg0",
+      logins: [
+        {title: "Agent", userid: "abbybanksy@broker.ca", password: "password"},
+        {title: "Seller", userid: "louiekritski@fake.com", password: "password"}
+      ]
     },
     {
-      title: "TORONTO AREA RESOURCES", 
+      title: "Toronto Area Resources", 
       summary: "Query the City of Toronto data to find out what resources exist and plot them on an interactive map.", 
       skills: ['MapBox', 'HTML', 'CSS', 'JavaScript', 'AJAX', 'City of Toronto Open Data API'], 
       github: "https://github.com/ruthtech/torontoAreaResources", 
       heroku: "https://ruthtech.github.io/torontoAreaResources",
       key: 2,
-      colour: "bg-medium"
+      colour: "bg-mediumBlue",
+      image: {PrjImg1}
     },
     {
-      title: "EMPLOYEE TRACKER", 
+      title: "Employee Tracker", 
       summary: "Command line tool that manages a company's employees using node, inquirer, and MySQL.", 
       skills: ['Node', 'Inquirer', 'JavaScript', 'MySQL' ], 
       github: "https://github.com/ruthtech/employee-tracker", 
       heroku: "https://github.com/ruthtech/employee-tracker/blob/master/employee-tracker.gif",
       key: 3,
-      colour: "bg-dark"
-    }
-  ];
-
-  const skillSets = [
-    {
-      title: "Programming",
-      skills: ["JavaScript", "Java", "SQL", "React", "Node"]
-    },
-    {
-      title: "Tests and Tools",
-      skills: ["Git", "JUnit", "JEST", "Visual Code", "Eclipse"]
-    },
-    {
-      title: "Soft skills",
-      skills: ["Organized", "Reliable", "Problem solver", "Strong work ethic", "Thorough"]
+      colour: "bg-darkBlue",
+      image: {PrjImg2}
     }
   ];
 
   return (
     <div className="container-fluid">
       <div className="row display-flex">
+        <ProjectDetails project={projects[activeProjectIndex]} />
+      </div>
+      <div className="row display-flex bottom-row">
         <ProjectCard projects={projects}/>
       </div>
-      <div className="row bottom-row">
-        <SkillsCard skillSets={skillSets} />
+    </div>
+  );
+}
+
+function ProjectDetails(props) {
+  console.log(props);
+  console.log(props.project.image);
+  console.log(props.project.image.refs);
+  console.log(props.refs);
+
+  return (
+    <div className="details definition d-flex">
+      <div className="col-12 col-sm-5 details-text">
+        <h2>{props.project.title}</h2>
+        <p>{props.project.summary}</p>
+        <ul className="project-skills">{props.project.skills.map( (skill, index) => {
+          return <li key={'detail'+index}>{skill}</li>
+        })}</ul>
+        Logins to use in the demo:
+        <ul className="login">{props.project.logins.map( (login, index) => {
+          return <li key={'login'+index}>{login.title}: <span className="loginliteral">{login.userid}</span>, password: <span className="loginliteral">{login.password}</span></li>
+        })}</ul>
+        <div className="row">
+          <div className="col">
+            <button
+              type="button"
+              className="btn portfolio-button"
+              onClick={() => {window.location=props.project.github}}>
+                See the code
+            </button>
+          </div>
+          <div className="col">
+            <button
+              type="button"
+              className="btn portfolio-button"
+              onClick={() => {window.location=props.project.heroku}}>
+                View demo
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="col-12 col-sm-7 details-image">
+        {/* <img src={require(props.project.image)} alt={props.project.title} /> */}
+        <img src={props.project.image.PrjImg0} alt={props.project.title} />
       </div>
     </div>
   );
@@ -64,7 +107,7 @@ function ProjectCard(props) {
     props.projects.map( (project, index) => {
     return (
       <div className="col-12 col-sm-4 mt-3" key={'project'+index}>
-        <div className="card card-portfolio box h-100">
+        <div className="card card-portfolio box">
           <div className="card-header portfolio-header">
             <h5 className="card-title">{project.title}</h5>
           </div>
@@ -90,26 +133,6 @@ function ProjectCard(props) {
         </div>
       </div>
     )}));
-}
-
-function SkillsCard(props) {
-  return (
-    props.skillSets.map( (skillSet, index) => {
-      return (
-          <div className="col-12 col-sm-4" key={'skillSet'+index}>		
-            <div className="card skills-card">
-              <h5 className="card-header">{skillSet.title}</h5>
-              <div className="card-body">
-                <ul className="card-text pl-3">
-                  {skillSet.skills.map((skill, index2) => {
-                    return <li key={'skill'+index2}>{skill}</li>;
-                  })}
-                </ul>
-              </div>
-            </div>
-          </div>
-      )})
-  );
 }
 
 export default Home;
